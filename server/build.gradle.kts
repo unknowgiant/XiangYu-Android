423dca0a2761341a4fc4b8b347b0980af8259b6e
unknowgiant <unknowgiant@users.noreply.github.com>
2026-08-18 08:00:29 +0800

    修复后端构建仓库配置

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -13,10 +13,6 @@ java {
     }
 }
 
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
